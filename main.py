70966cf4e0a91e224a8455d34753310b36d87e30
David Satomi <d.satomi@outlook.de>
2023-11-11 11:27:26 +0100

    Improved debate architecture

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate  
-from langchain.schema import ChatMessage
-
 import streamlit as st
 
 from utils.StreamHandler import StreamHandler
-from utils.WorkerManager import WorkerManager
 from utils.Debate import Debate
 
 def initialize():
@@ -20,9 +15,8 @@ def initialize():
         st.info("Enter an OpenAI API Key to continue")
         st.stop()
 
-    st.session_state["worker_manager"] = WorkerManager(openai_api_key=st.session_state["openai_api_key"], model_name="gpt-3.5-turbo")
     st.session_state["experts"] = []
-    st.session_state["messages"] = Debate()
+    st.session_state["debate"] = Debate(openai_api_key=st.session_state["openai_api_key"], model_name="gpt-3.5-turbo")
     
 
 st.markdown(
@@ -65,38 +59,37 @@ options = ["Strongly For", "For", "Neutral", "Against", "Strongly Against"]
 stance = expander.select_slider("Stance of the experts", options=options, value="Neutral")
 submitted = buttonCol.form_submit_button(label="Submit", on_click=initialize)
 
+chat = st.container()
+chat_options = st.container()
 
 
-if "messages" in st.session_state:
-    for msg in st.session_state.messages.debate_history:
-        st.chat_message(name=msg["role"], avatar=msg["avatar"]).write(msg["content"])
-
+if "debate" in st.session_state:
+    for msg in st.session_state.debate.debate_history:
+        chat.chat_message(name=msg["role"], avatar=msg["avatar"]).write(msg["content"])
 
 
 def debate_round():
     for expert in st.session_state["experts"]:
-        with st.chat_message(name=expert.expert_instruction["role"], avatar=expert.expert_instruction["avatar"]):
-            response = expert.generate_argument(st.session_state.messages, StreamHandler(st.empty()))
-            st.session_state.messages.add_message(role=expert.expert_instruction["role"], avatar=expert.expert_instruction["avatar"], content=response)
+        with chat.chat_message(name=expert.expert_instruction["role"], avatar=expert.expert_instruction["avatar"]):
+            response = expert.generate_argument(st.session_state.debate, StreamHandler(st.empty()))
+            st.session_state.debate.add_message(role=expert.expert_instruction["role"], avatar=expert.expert_instruction["avatar"], content=response)
 
 if submitted and topic.strip() != "":
-    st.session_state.messages.add_message(role="user", content=topic)
-    st.session_state.messages.set_topic(topic)
-    st.chat_message("user").write(topic)
-    st.session_state["experts"] = st.session_state["worker_manager"].create_experts(num_experts=number_experts, topic=topic)
+    st.session_state.debate.add_message(role="user", content=topic)
+    # TODO: set header title here with callback
+    chat.chat_message("user").write(topic)
+    st.session_state["debate"].initialize_new_debate(topic=topic, num_experts=number_experts)
+    st.session_state["experts"] = st.session_state["debate"].get_experts()
 
     debate_round()
 
 if "initialized" in st.session_state and st.session_state["initialized"]:
-
-    if input := st.chat_input(placeholder="Participate in the debate"):
+    if input := chat_options.chat_input(placeholder="Participate in the debate"):
         user_prompt = input.replace("Human: ", "")
-        st.session_state.messages.add_message(role="user", content=user_prompt)
-        #st.session_state.messages.append(ChatMessage(role="user", content=user_prompt))
-        st.chat_message("user").write(user_prompt)
-        #experts = worker_manager.create_experts(num_experts=number_experts, topic=user_prompt)
+        st.session_state.debate.add_message(role="user", content=user_prompt)
+        chat.chat_message("user").write(user_prompt)
 
         debate_round()
 
-    st.button("Next round of debate")
+    chat_options.button("Next round of debate", on_click=debate_round)
 
--- utils/Debate.py
+++ utils/Debate.py
@@ -1,11 +1,18 @@
 from langchain.schema import ChatMessage
+from langchain.chat_models import ChatOpenAI
+from utils.Coordinator import Coordinator
+from utils.Expert import Expert
+
 
 
 class Debate():
-    def __init__(self):
+    def __init__(self, openai_api_key, model_name="gpt-3.5-turbo"):
+        self.openai_api_key = openai_api_key
+        self.model_name = model_name
         self.topic = None
         self.debate_history = []
         self.memory = []
+        self.experts = []
 
     def add_message(self, role, content, avatar=None):
         self.debate_history.append({"role": role, "avatar": avatar, "content": content})
@@ -13,5 +20,30 @@ class Debate():
         self.memory.append(ChatMessage(role=role, content=content))
 
 
-    def set_topic(self, topic):
+    def get_experts(self):
+        return self.experts
+
+    def initialize_new_debate(self, topic, num_experts):
         self.topic = topic
+        expert_instructions = self.get_expert_instructions(num_experts)
+        self.experts = self.generate_experts(expert_instructions)
+
+    def initialize_existing_debate(self, topic, debate_history, expert_instructions):
+        self.topic = topic
+        self.debate_history = debate_history
+        for message in debate_history:
+            role = "user" if message["role"] == "user" else "assistant"
+            self.memory.append(ChatMessage(role=role, content=message["content"]))
+        self.experts = self.generate_experts(expert_instructions)
+
+    def get_expert_instructions(self, num_experts):
+        coordinator_model = ChatOpenAI(openai_api_key=self.openai_api_key, model_name=self.model_name)
+        coordinator = Coordinator(coordinator_model, num_experts, self.topic)
+        return coordinator.generate_expert_instructions()
+
+    def generate_experts(self, experts_instructions):
+        experts = []
+        for expert_instruction in experts_instructions:
+            expert_model = ChatOpenAI(openai_api_key=self.openai_api_key, model_name=self.model_name, streaming=True)
+            experts.append(Expert(expert_model, expert_instruction))
+        return experts
